139e2347aa104596156e8ce5568e2713762f9e1e
Ditone <qcreidy@gmail.com>
2021-03-07 01:48:09 -0600

    Main now is just to test code.
    Added basic functionality to getInfoFromMatchID.py.

--- SupportingScripts/getInfoFromMatchID.py
+++ SupportingScripts/getInfoFromMatchID.py
@@ -1,39 +1,75 @@
-# The purpose of this file is to test my connectivity to the Steamworks dev API
-# Also looking test the GetMatchDetails call, and experiment with the returned data type.
-# Will most likely return the dataset to a local file so that I'm not spamming the network.
-#
-# IDOTA2Match_<ID>
-# URL: GET http://api.steampowered.com/IDOTA2Match_<ID>/GetMatchDetails/v1
-#
-# match_id (string)
-#   Match id
-#
-# Your Steam Web API Key
-# Key: 66C3D405427A17EECDA4280DA8D65851
-# Domain Name: https://quinreidy.me
+# Point of this file is to store functionality of retrieving match information, and storage options
+# No analysis here, just the retrieval and storage
 
-# going to start with one specific match - EG vs Quincy Crew, Tie Breakers
+import requests
+import json
+
+
+# Default get_specific_match will specifically get QC vs EG, NA DPC S1 Group Stage Tiebreakers
+# Encapsulated the request call here so that I don't have to run too many requests
+# returns a JSON of the match details to be filtered
 def get_specific_match():
-    status = 0
+    # url for the API request
+    get_qc_eg_url = "https://api.opendota.com/api/matches/5861478139?api_key=3ca47c01-644c-48fc-b81c-ca2032313edc"
 
-    # open file
+    # request_test = requests.get(url, headers=headers)
     try:
-        output = open("SupportingScripts/match_output", "w")
-        status = 1
+        request_test = requests.get(get_qc_eg_url).json()
+        json_results = json.dumps(request_test, indent=4)
 
-    except FileNotFoundError:
-        print("Couldn't find file")
-        status = 0
+    except IOError as io_err:
+        print("Something went wrong with the request: ", io_err)
 
-    if status == 0:
-        return False
+    return json_results
 
-    # write to the file
-    output.write("match details")
 
-    # close file
-    output.close()
+# Input: int Match ID
+# Output: json results
+def get_specific_match(match_id: int):
+    # url for the api request
+    get_url = "https://api.opendota.com/api/matches/" + str(match_id) + "?api_key=3ca47c01-644c-48fc-b81c-ca2032313edc"
 
-    # return true for complete
-    return True
+    # request_test = requests.get(url, headers=headers)
+    try:
+        request_test = requests.get(get_url).json()
+        json_results = json.dumps(request_test, indent=4)
 
+    except IOError as io_err:
+        print("Something went wrong with the request: ", io_err)
+
+    return json_results
+
+
+# Will take an input (type json) and write it to a file
+# Default is a file named "match_output" found in the SupportingScripts folder
+# Eventually will use helper methods to filter out the relevant data
+def write_match_to_file(result_set: json):
+
+    if result_set:
+
+        # dumping the result
+        json_results = json.dumps(result_set, indent=4)
+
+        # attempt to open the file / open the file
+        try:
+            results_file = open("SupportingScripts/match_output", 'w')
+
+        except FileNotFoundError:
+            print("Couldn't find the file")
+            exit(666)
+
+        # clear the file before adding
+        print("clearing file")
+        results_file.write("")
+
+        # writing to the file
+        print("writing to file")
+        results_file.write(json_results)
+
+        # close the file
+        print("finished writing, closing")
+        results_file.close()
+
+    else:
+        print("No valid input or result set")
+        exit(666)
--- SupportingScripts/main.py
+++ SupportingScripts/main.py
@@ -26,37 +26,8 @@ import json
 
 
 def main():
-    print("STEAM WEB API KEY: 66C3D405427A17EECDA4280DA8D65851")
-    print("The domain I used: https://quinreidy.me")
-
-    # Authorization Header
-    test_url = "https://api.opendota.com/api/matches/5861478139?api_key=3ca47c01-644c-48fc-b81c-ca2032313edc"
-    url = "https://api.opendota.com/api/matches/5861478139"
-    headers = {'Authorization: Bearer 3ca47c01-644c-48fc-b81c-ca2032313edc'}
-
-    # request_test = requests.get(url, headers=headers)
-    request_test = requests.get(test_url).json()
-
-    if request_test:
-
-        # dumping the result
-        json_results = json.dumps(request_test, indent=4)
-
-        # open the file
-        try:
-            results_file = open("SupportingScripts/match_output", 'w')
-
-        except FileNotFoundError:
-            print("Couldn't find the file")
-            exit(666)
-
-        # clear the file
-        print("clearing file")
-        results_file.write("")
-
-        # writing to the file
-        print("writing to file")
-        results_file.write(json_results)
+    match_id = 1
+    print("https://api.opendota.com/api/matches/" + str(match_id) + "?api_key=3ca47c01-644c-48fc-b81c-ca2032313edc")
 
 
 if __name__ == "__main__":
